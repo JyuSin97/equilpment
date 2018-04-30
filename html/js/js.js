ed632f30b442d5554ed77fe3a8edf804d287b830
JyuSin97 <ivy87630@gmail.com>
2018-04-30 16:32:01 +0800

    2

---
+++ html/js/js.js
@@ -0,0 +1,11 @@
+/* DEMO2 */
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:2,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[767,1],
+        pagination: true,
+        autoPlay:true
+    });
+});
